b660ed4e4866bde19263d07753221d2c102974f6
Clara <myathetchel572@gmail.com>
2025-02-19 16:33:07 +0700

    save data and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,13 @@
 export let cart = JSON.parse(localStorage.getItem("cart")) || [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
+    quantity: 1,
+    deliveryOptionId: "1",
   },
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 3,
+    quantity: 2,
+    deliveryOptionId: "2",
   },
 ];
 
@@ -28,10 +30,21 @@ export function addToCart(productId, quantity) {
     cart.push({
       productId,
       quantity: quantity,
+      deliveryOptionId: "1",
     });
   }
 
   saveToStorage();
+}
+
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((item) => {
+    cartQuantity += item.quantity;
+  });
+
+  return cartQuantity;
 }
 
 export function removeFromCart(productId) {
@@ -43,5 +56,21 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+  saveToStorage();
+}
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+
+  console.log(matchingItem.quantity);
+
   saveToStorage();
 }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -66,12 +66,7 @@ document.querySelector(".products-grid").innerHTML = productsHTML;
 updateCartQuantity();
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((item) => {
-    cartQuantity += item.quantity;
-  });
-
+  const cartQuantity = calculateCartQuantity();
   document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 }
 
